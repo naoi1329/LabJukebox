ff2df0d1e7e12eb36ac68c72d7f2a93b76d6b75c
naoi1329 <x15071xx@aitech.ac.jp>
2018-04-13 12:07:33 +0900

    Initial Commit

---
+++ LabJukebox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LabJukebox
+//
+//  Created by 直井翔汰 on 2018/04/13.
+//  Copyright © 2018年 直井翔汰. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LabJukebox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LabJukebox
+//
+//  Created by 直井翔汰 on 2018/04/13.
+//  Copyright © 2018年 直井翔汰. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
